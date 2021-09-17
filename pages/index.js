88bf7c700817c9ca666ac5f925d0bca10b418404
Jessica Moore <jessica.cmoore1@gmail.com>
2021-09-17 19:10:56 -0400

    Home layout skeleton

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Head from 'next/head'
+import LayoutHome from '../layouts/Home'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-    </div>
+    <LayoutHome>
+      <h1>Screenreader Obstacle Course</h1>
+    </LayoutHome>
   )
 }
